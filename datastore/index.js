79d080a63c0ae1a2589dbceadb09c514d82f90ff
Kim Honrada <kimhonrada21@gmail.com>
2021-10-11 21:27:48 -0400

    Complete deleteing a todo

--- datastore/index.js
+++ datastore/index.js
@@ -63,24 +63,30 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, fileData) => { // <--- directory, using the right id
+    if (err) {
+      callback(new Error(`No file with this id: ${id}`));
+    } else {
+      fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
+        if (err) {
+          throw ('could not update file');
+        } else {
+          callback(null, {id, text: text});
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(path.join(exports.dataDir, `${id}.txt`), (err) => {
+    if (err) {
+      callback(new Error(`No file with this id: ${id}`));
+    } else {
+      //should delete file;
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
